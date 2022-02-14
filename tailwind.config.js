dfa6dedd0e3bbf3edf2ae3e05f04c4a28172ef9a
shayor007 <shayofunmiojomu@gmail.com>
2022-02-14 16:16:13 +0100

    theme changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      width: {
+        '128': '32rem',
+      },
+      colors: {
+        'mycolor1': '#05386B',
+        'mycolor2': '#8EE4AF',
+        'mycolor3': '#EDF5E1'
+      }, 
       backgroundImage: theme => ({
         'car': "url('./assets/images/emma.jpg')"
       }),
